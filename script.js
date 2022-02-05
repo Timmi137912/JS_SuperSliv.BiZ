0fd526880ee53981a8e4a8a9d2cbbbd35cf032a8
Timmi <z1k11@rambler.ru>
2022-02-05 18:20:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("hellp");
+
+// let answer = confirm("are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("18 есть?", "да");
+
+// console.log(typeof (answer));
+
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
