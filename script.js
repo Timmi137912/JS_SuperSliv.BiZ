ae2d91391c0a25c74a55b928bdd603450fd33b14
Timmi <z1k11@rambler.ru>
2022-02-06 07:29:08 +0300

    new

--- script.js
+++ script.js
@@ -1,59 +1,14 @@
 'use strict';
 
-// var leftBorderWidth = 1;
-// let second = 2;
-// const pi = 3.14;
+let answer = money("Ваш бюджет на месяц?");
 
-var number = 5;
-var string = "Hello";
-var sym = Symbol();
-null;
-undefined;
-var obj = {};
+let answer = data("Введите дату в формате YYYY-MM-DD");
 
-console.log(4 / 0);
-console.log('string' * 9);
+var budget = money;
+var timeData = data;
+var expenses;
+var optionalExpenses;
+var income;
+var savings = false;
 
-let something;
-console.log(something);
-
-let persone = {
-    name: "john",
-    age: 25,
-    isMarried: false
-};
-
-console.log(persone["name"]);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-
-console.log(arr[0]);
-
-// alert("hellp");
-
-// let answer = confirm("are you here?");
-
-// console.log(answer);
-
-// let answer = +prompt("18 есть?", "да");
-
-// console.log(typeof (answer));
-
-
-// console.log("arr" + " - object");
-// console.log(4 + " - object");
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5 % 2);
-
-console.log("2" === 2);
-
-let isChecked = false,
-    isClose = false;
-
-console.log(isChecked || !isClose);
+let answer = data("Введите дату в формате YYYY-MM-DD");
